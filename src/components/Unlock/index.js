70dfc230847e69f81c6f07a6be841ac0f5d54acb
KalyanRaj <115339023+kalyanRaj92@users.noreply.github.com>
2023-08-22 18:20:45 +0530

    Update index.js

--- src/components/Unlock/index.js
+++ src/components/Unlock/index.js
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+/* import {useState} from 'react'
 
 import {
   MainContainer,
@@ -41,4 +41,42 @@ const Unlock = () => {
     </MainContainer>
   )
 }
+export default Unlock  */
+
+
+import {useState} from 'react'
+
+import {
+  UnlockLockContainer,
+  ImageAndTextContainer,
+  Image,
+  Text,
+  Button,
+} from './styledComponents'
+
+const Unlock = () => {
+  const [isUnlocked, setIsUnlocked] = useState(false)
+  const imageUrl = isUnlocked
+    ? 'https://assets.ccbp.in/frontend/hooks/unlock-img.png'
+    : 'https://assets.ccbp.in/frontend/hooks/lock-img.png'
+  const altText = isUnlocked ? 'unlock' : 'lock'
+  const text = isUnlocked ? 'Your Device is Unlocked' : 'Your Device is Locked'
+  const buttonText = isUnlocked ? 'Lock' : 'Unlock'
+
+  const onClickButton = () => setIsUnlocked(prevStatus => !prevStatus)
+
+  return (
+    <UnlockLockContainer>
+      <ImageAndTextContainer>
+        <Image src={imageUrl} alt={altText} />
+        <Text>{text}</Text>
+      </ImageAndTextContainer>
+      <Button type="button" onClick={onClickButton}>
+        {buttonText}
+      </Button>
+    </UnlockLockContainer>
+  )
+}
+
 export default Unlock
+
